49cfaea1a55ad4dc018088d8e9583b8ad9c517ee
Dev Sadisatsowala <117472132+Dev750wala@users.noreply.github.com>
2024-03-28 04:02:32 +0530

    first version

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Right_Div from './components/Right/Right_Div';
 
 function App() {
   return (
-    <div className="container mx-auto flex flex-col lg:flex-row justify-evenly mt-24 lg:mt-28 bg-transparent">
+    <div className="container mx-auto flex flex-col lg:flex-row justify-between mt-24 lg:mt-28 bg-transparent">
       <Left_Div />
       <Right_Div />
     </div>
