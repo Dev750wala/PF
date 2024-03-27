153edafd2932a21893aa48b7cb8bf020022ec56c
Dev Sadisatsowala <117472132+Dev750wala@users.noreply.github.com>
2024-03-28 04:11:22 +0530

    first version

--- src/components/Left/Left_Div.jsx
+++ src/components/Left/Left_Div.jsx
@@ -5,7 +5,7 @@ import Left2 from './Left2';
 
 const Left_Div = () => {
   return (
-    <div className="lg:fixed lg:w-1/3 md:w-1/2 w-full px-2.5 lg:px-16 lg:pl-4 bg-transparent z-10 mr-2 lg:mr-5">
+    <div className="lg:fixed lg:w-1/3 md:w-1/2 w-full px-2.5 lg:px-16 bg-transparent z-10 mr-2 lg:mr-5">
       <Left1 />
       <Left2 />
     </div>
