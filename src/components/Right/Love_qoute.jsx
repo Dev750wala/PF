2aa1f163265195682520661d22b1d25de0120141
Dev Sadisatsowala <117472132+Dev750wala@users.noreply.github.com>
2024-06-28 23:22:45 +0530

    second commit

--- src/components/Right/Love_qoute.jsx
+++ src/components/Right/Love_qoute.jsx
@@ -6,7 +6,7 @@ const Love_qoute = () => {
     <div className='bg-transparent pl-4 pr-5 lg:pl-12 mb-20 mt-28'>
       <p className='bg-transparent text-sm text-gray-400 mb-10'>
         Made with 💖 and with the blessings of <b className='bg-transparent'>Hari</b>🦚. Coded in <b className='bg-transparent'>Visual Studio Code</b> by yours truly. Built with <b className='bg-transparent'>React.js</b> and <b className='bg-transparent'>Tailwind CSS</b> and deployed with <b className='bg-transparent'>Netlify</b>.
-      </p><br /><br />
+      </p><br />
 
       <p className='bg-transparent text-sm text-gray-400 my-10 mt-7'>
         <b className='bg-transparent'>Note: </b>Updates to this portfolio are ongoing.
