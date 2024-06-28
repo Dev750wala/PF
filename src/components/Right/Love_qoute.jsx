5961bc8f7a508a6c5cd4b8d41b2da881a00874db
Dev Sadisatsowala <117472132+Dev750wala@users.noreply.github.com>
2024-06-28 23:04:10 +0530

    second commit

--- src/components/Right/Love_qoute.jsx
+++ src/components/Right/Love_qoute.jsx
@@ -4,7 +4,7 @@ import { Love_Ending } from '../../Data'
 const Love_qoute = () => {
   return (
     <div className='bg-transparent pl-4 pr-5 lg:pl-12 mb-20 mt-28'>
-      <p className='bg-transparent text-sm text-gray-400'>
+      <p className='bg-transparent text-sm text-gray-400 mb-10'>
         Made with 💖 and with the blessings of <b className='bg-transparent'>Hari</b>🦚. Coded in <b className='bg-transparent'>Visual Studio Code</b> by yours truly. Built with <b className='bg-transparent'>React.js</b> and <b className='bg-transparent'>Tailwind CSS</b> and deployed with <b className='bg-transparent'>Netlify</b>.
       </p>
 
